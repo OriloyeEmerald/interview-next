33c4d2499e719d1f212a54357357ddcf4bfde853
Emerald Oriloye <oluwapelumibalogun2@gmail.com>
2023-08-16 09:21:24 -0700

    updated files

--- app/layout.js
+++ app/layout.js
@@ -12,11 +12,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-       
-      <body className={inter.className}>
-      <Navbar />
-        <Home />
-      </body>
+       <Navbar />
+      <body className={inter.className}>{children}</body>
     </html>
   )
 }
