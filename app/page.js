c4a53d469b487ca2fadf376b9c97d0f0936c6367
Emerald Oriloye <oluwapelumibalogun2@gmail.com>
2023-08-16 11:17:59 -0700

    update files

--- app/page.js
+++ app/page.js
@@ -2,29 +2,32 @@
 export default function Home() {
   return (
     <main className="App">
-      <div>
-      <div>
-        <h3>We've Missed You</h3>
+      <div className="intro">
+        <h2>We've Missed You</h2>
         <br />
-        <p>Kindly sign in and get first access to the very best community and unlock more opportunities</p>
+        <p className="signin-text">Kindly sign in and get first access to the very best, community and unlock more opportunities.</p>
         <br />
         <form>
           <div>
-            <div>Email:</div>
+            <div className="p">Email:</div>
             <input type="text"/></div>
           <br />
           <div>
-            <div>Password:</div>
+            <div className="p">Password:</div>
             <input type="password"/></div>
           <br />
           <button className="btn">Login</button>
         </form>
-        <p>Forgot username or password?</p>
-        <p>Don't have an account? <span><a href="#">Sign up</a></span></p>
+        <br />
+        <p className="p">Forgot username or password?</p>
+        <br />
+        <p className="p">Don't have an account? <span><a href="#">Sign up</a></span></p>
       </div>
-
       
-      </div>
+      <div>
+        <img src="./newImg.jpg" alt="" />
+        </div>
+      
     </main>
   )
 }
